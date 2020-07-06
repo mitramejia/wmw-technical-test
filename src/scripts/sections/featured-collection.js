60e650d317580eb5449ddae54848b04dc142b8a3
Mitra Mejia <mmejia@wunderkind.nyc>
2020-07-06 00:06:11 -0400

    Tweak flickity's next/prev svg to match design

--- src/scripts/sections/featured-collection.js
+++ src/scripts/sections/featured-collection.js
@@ -31,6 +31,12 @@ register('featured-collection', {
       groupCells: true,
       cellAlign: 'left',
       contain: true,
+      arrowShape: {
+        x0: 10,
+        x1: 60, y1: 50,
+        x2: 65, y2: 45,
+        x3: 20,
+      },
     });
 
 
